ae605957e0c385c78c1968a593696d28ab0825c6
Grzegorz Trochonowicz <trotylpl@gmail.com>
2025-01-12 18:37:59 +0000

    terraform config to correct

--- test-cloudinit.tf
+++ test-cloudinit.tf
@@ -88,6 +88,7 @@ resource "proxmox_vm_qemu" "cloudinit-k3s-worker" {
     # Keep in mind to use the CIDR notation for the ip.
     ipconfig0 = "ip=192.168.3.11${count.index + 1}/24,gw=192.168.3.1"
     ciuser = "ubuntu"
+    # public key to accress by ssh
     sshkeys = <<EOF
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKfn+iQH2HnuXIa67zBzEUfrg4mfH9meVy/CWuzODj2G8K6xWXUx9YTzchqr2xjXxLDQcDC+RDwVY0cAiWZ/OrXd6H8QEkSSEuAc/exWvVmLQGr3e41Ff9BUslqayGEPwFP6fndCWK8FthiwYUUGM5sODLZl4DFcqtgICRzuEcNcRQZFpq/h5NzCYmdPWMrAuQLfF/fW2VRD5gnqaDkgW+pdw+1umut5ey6C4fCPA9/7vkSesDc2RafA3gRkBmO1IxBVhzgl7OLl84iFnl1dEUcrdSCoaRL2AHC36EDvhF5M/Zlv5hebITzc9f7cF88k/rsx2ZnlSOeIHD31/lRhWL root@proxmox-dell
     EOF
